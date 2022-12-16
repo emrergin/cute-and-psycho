0fc8fa0d9e35dd295b81920919ba2e309b4212be
emrergin <emrergin2757@yahoo.com>
2022-12-16 10:03:39 +0300

    layout cleanup

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar">
-    <div class="d-flex justify-content-between mw-100" id="navbarsubbox">
-            <a class="susluyazi navbar-brand hugetext" href="/">
+    <div id="navbarsubbox">
+            <a class="susluyazi navbar-brand hugetext" id="banner-name" href="/">
                 Sena Ergin
             </a>
 
@@ -17,7 +17,7 @@
                 <a class="navbar-brand invis-when-small" href="/#iletisim">
                     İletişim
                 </a>
-                <a aria-label="İnstagram gönderilerim" href="https://www.instagram.com/psk.senaergin/" target="_blank" rel="noreferrer">
+                <a id="instagram-link" aria-label="İnstagram gönderilerim" href="https://www.instagram.com/psk.senaergin/" target="_blank" rel="noreferrer">
                     <svg width="2.5em" height="2.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"></path></svg>
                 </a>
 
@@ -63,6 +63,10 @@
         margin-bottom:2ch;
     }
 
+    #banner-name{
+        max-height:150px;
+    }
+
     .navbar{          
         display:flex;
         justify-content: space-around;
@@ -105,11 +109,18 @@
         justify-content:center;
     }
     #navbarsubsubbox{
-        gap:20px;
+        gap:30px;
         margin-inline: auto;
     }
     .invis-when-small{
         display:none;
+    }
+    #instagram-link{
+        order:-1;
+    }
+    #banner-name{
+        padding-bottom:1rem;
+        max-height:134px;
     }
 }
 
