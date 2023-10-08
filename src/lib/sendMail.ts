ce26958b11bdf458097e0b506963590961451c9b
Emre Ergin <emrergin2757@yahoo.com>
2023-10-08 21:43:05 +0300

    language detection for emails

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,35 +1,49 @@
 import nodemailer from "nodemailer";
-import { env } from '$env/dynamic/private';
+import { env } from "$env/dynamic/private";
+
+// const LanguageDetect = import "require('languagedetect');
+
+import LanguageDetect from "languagedetect";
+const lngDetector = new LanguageDetect();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com' ,
-    port: 587,
-    secure: false,
-    auth: {
-      user: env.EMAIL,
-      pass: env.PSKTOKEN,
-    },
-    pool: true,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: env.EMAIL,
+    pass: env.PSKTOKEN,
+  },
+  pool: true,
 });
 
-const sendMail =  async (name: string, email: string, message: string,phone:string) => {
-
+const sendMail = async (
+  name: string,
+  email: string,
+  message: string,
+  phone: string
+) => {
+  if (
+    lngDetector.detect(message)[0][0] !== "turkish" &&
+    lngDetector.detect(message)[0][1] > 0.5
+  ) {
+    return;
+  }
   const mailOptions = {
-      from: env.EMAIL,
-      to: env.EMAIL,
-      subject: `Mesaj-${name}-${email}`,
-      text: message+'\n\nTelefon numarası: '+phone, 
-      replyTo:email     
+    from: env.EMAIL,
+    to: env.EMAIL,
+    subject: `Mesaj-${name}-${email}`,
+    text: message + "\n\nTelefon numarası: " + phone,
+    replyTo: email,
   };
 
   const res = await transporter.sendMail(mailOptions);
 
-  if(res.response.slice(0,3)==='250'){
-    return {success: true};
+  if (res.response.slice(0, 3) === "250") {
+    return { success: true };
+  } else {
+    return { error: true };
   }
-  else{
-    return {error: true};
-  }
-}
+};
 
 export default sendMail;
