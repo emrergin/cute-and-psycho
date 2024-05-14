0aafd7ed301cf336282c337fcfdaac954c9c3d7e
Emre Ergin <emrergin2757@yahoo.com>
2024-05-14 10:55:16 +0300

    post update

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,7 +1,7 @@
-import * as api from "$lib/api.js";
+import { getPost } from "$lib/api";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async function ({ params }) {
-  const { props } = await api.getSingleArticle(params.slug);
-  return props;
+  const article = await getPost(Number(params.slug));
+  return article;
 };
